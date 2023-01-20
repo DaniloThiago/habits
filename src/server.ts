ad84b502d9cb6e2cd75a8fa07e3d9c841518051c
Danilo Oliveira <danilo.t.a.oliveira@gmail.com>
2023-01-20 20:08:59 -0300

    :arrow_up: Install Prisma and config model Habit

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log('oi2')
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return "Connected";
+});
+
+app.listen({ port: 3333 }).then(() => console.log("HTTP Server running!"));
